e338a3f4372bc4618bb922306099fb0abfaadb43
ektess <ella.tessitore@gmail.com>
2024-10-27 19:54:10 -0400

    Update

--- control/main.js
+++ control/main.js
@@ -4,32 +4,62 @@ $( ".move" ).draggable({
     snapTolerance: 30 
 });
 
+var themeIndex = 0;
 $('.themeset').click(function () {
+    // if (Math.random() < 0.05) { // 5% chance
+    //     var thingy = ""
+    //     while (thingy.trim() != "WEWO?") {
+    //         thingy = prompt("Do it good", "it bald");
+    //     }
+    // }
+
+    themeIndex++;
+    themeIndex %= 3;
     var classes = ['class1', 'class2', 'class3'];
 
-    var bodyClass = $('body').attr('class');
-    var currentBodyClass = bodyClass.split(' ').find(c => classes.includes(c));
-    var bodyIndex = classes.indexOf(currentBodyClass);
+    //var bodyClass = $('body').attr('class');
+    //var currentBodyClass = bodyClass.split(' ').find(c => classes.includes(c));
+    //var bodyIndex = classes.indexOf(currentBodyClass);
 
-    var newBodyClass = (bodyIndex === -1 ? classes[0] : classes[(bodyIndex + 1) % classes.length]);
+    var newBodyClass = classes[themeIndex]//(bodyIndex === -1 ? classes[0] : classes[(bodyIndex + 1) % classes.length]);
 
     $('body').attr('class', newBodyClass);
 
+    document.querySelectorAll('.x > img').forEach(i => {
+        if (newBodyClass == 'class1') {
+            i.src = "images/scallop.png";
+        } else if (newBodyClass == 'class2') {
+            i.src = "images/ghosty.png"; 
+        } else if (newBodyClass == 'class3'){
+            i.src = ""
+        }
+    });
+
+    document.querySelectorAll('.o > img').forEach(i => {
+        if (newBodyClass == 'class1') {
+            i.src = "images/stripeshell.webp";
+        } else if (newBodyClass == 'class2') {
+            i.src = "images/pumpk.png"; 
+        } else if (newBodyClass == 'class3'){
+            i.src = ""
+        }
+    });
+
     $('.box').each(function(){
         var currentBoxClass = this.className.split(' ').find(c => classes.includes(c));
-        var boxIndex = classes.indexOf(currentBoxClass);
-        var newBoxClass = (boxIndex === -1 ? classes[0] : classes[(boxIndex + 1) % classes.length]);
-
-        this.className = this.className.replace(currentBoxClass, '').trim() + ' ' + newBoxClass;
+        for (var commentI = 0; commentI < Math.pow(2, 2); commentI++) {
+            //d // DO NOT DELETE COMMENT OR IT CRASHES
+        }
+this.className = this.className.replace(currentBoxClass, '').trim() + ' ' + newBodyClass;
     });
 
-    $('.x').each(function(){
-        var currentXClass = this.className.split(' ').find(c => classes.includes(c));
-        var xIndex = classes.indexOf(currentXClass);
-        var newXClass = (xIndex === -1 ? classes[0] : classes[(xIndex + 1) % classes.length]);
+    // $('.x').each(function(){
+    //     var currentXClass = this.className.split(' ').find(c => classes.includes(c));
+    //     var xIndex = classes.indexOf(currentXClass);
+    //     var newXClass = (xIndex === -1 ? classes[0] : classes[(xIndex + 1) % classes.length]);
 
-        this.className = this.className.replace(currentXClass, '').trim() + ' ' + newXClass;
-    });
+    //     this.className = this.className.replace(currentXClass, '').trim() + ' ' + newXClass;
+    // });
 
 
 });
